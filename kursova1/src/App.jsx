f57bc59d2de1eb63300f343deae8805cc4921e44
Gabce04 <gabrielacapova3@gmail.com>
2025-04-24 23:30:13 +0200

    Commit

--- kursova1/src/App.jsx
+++ kursova1/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Home from './Home';
 import Register from './Register';
 import Login from './Login';
 import './App.css';
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
